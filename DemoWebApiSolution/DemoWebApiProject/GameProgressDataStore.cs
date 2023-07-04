63bde08134627ef6c900af4414c3d3c897765508
Charles Shu <cshu@planittesting.com>
2023-07-04 20:51:28 +1200

    learning progress: Demo: Improving the Architecture with Model Classes

---
+++ DemoWebApiSolution/DemoWebApiProject/GameProgressDataStore.cs
@@ -0,0 +1,31 @@
+﻿using DemoWebApiProject.Models;
+
+namespace DemoWebApiProject
+{
+    public class GameProgressDataStore
+    {
+        public List<GameProgressDto> GameProgresses { get; set; }
+        public static GameProgressDataStore Instance { get; } = new GameProgressDataStore(); // this is a "singleton pattern"
+
+        public GameProgressDataStore()
+        {
+            // initiate dummy data
+            GameProgresses = new List<GameProgressDto>() {
+                new GameProgressDto()
+                {
+                    Id = 1,
+                    Name = "心灵终结 Mental Omega",
+                    ChineseName = "心灵终结",
+                    EnglishName = "Mental Omega"
+                },
+                new GameProgressDto()
+                {
+                    Id = 2,
+                    Name = "tormented souls",
+                    ChineseName = "折磨之魂",
+                    EnglishName = "tormented souls"
+                }
+            };
+        }
+    }
+}
---
+++ DemoWebApiSolution/DemoWebApiProject/Models/GameProgressDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoWebApiProject.Models
+{
+    public class GameProgressDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? ChineseName { get; set; }
+        public string? EnglishName { get; set; }
+
+    }
+}
