389de43285d4b8249691df9dad0c81e9dddf4358
Charles Shu <charles.shu@datacom.co.nz>
2023-08-23 09:50:12 +1200

    add new files GameProgressRepository

---
+++ DemoWebApiSolution/DemoWebApiProject/Services/GameProgressRepository.cs
@@ -0,0 +1,34 @@
+﻿using DemoWebApiProject.DbContexts;
+using DemoWebApiProject.Entities;
+
+namespace DemoWebApiProject.Services
+{
+    public class GameProgressRepository : IGameProgressRepository
+    {
+        private readonly GameProgressContext _gameProgressContext;
+
+        public GameProgressRepository(GameProgressContext gameProgressContext)
+        {
+            _gameProgressContext = gameProgressContext ?? throw new ArgumentNullException(nameof(gameProgressContext));
+        }
+        public Task<GameProgress?> GetGameProgressAsync(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GameProgress>> GetGameProgressesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GameProgressOnPlatform>> GetGameProgressesOnPlatformsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GameProgressOnPlatform?> GetGameProgressOnPlatformAsync(int gameId, int gameProgressOnThisPlatformId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
